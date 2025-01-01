dd56ea3914f1b9e98fdca0b0e267af708a872369
Hossein <hossein.masihi@gmail.com>
2025-01-01 15:45:15 +0330

    models

--- scanner/forms/ip.py
+++ scanner/forms/ip.py
@@ -1,5 +1,12 @@
 from django import forms
 
+from scanner.models import PingScanReport
+
+
 class IPRange(forms.Form):
     start_ip = forms.GenericIPAddressField(label="Start IP")
     end_ip = forms.GenericIPAddressField(label="End IP")
+
+    class Meta:
+        model = PingScanReport
+        fields = ['start_ip', 'end_ip']
